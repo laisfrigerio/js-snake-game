10427f620ee391d308eef3c4663164de99b579fd
Lais Frigério <laisfrigerio@hotmail.com>
2020-06-11 13:50:15 -0300

    check game over
    Signed-off-by: Lais Frigério <laisfrigerio@hotmail.com>

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ function drawSnake () {
 
 function checkCollision () {
     if (snake[0].x > (15 * box) && direction == "right") snake[0].x = 0
-    if (snake[0].x < 0 && direction == "left") snake[0].x = (16 * box)
-    if (snake[0].y > (15 * box) && direction == "down") snake[0].y = 0
-    if (snake[0].y < 0 && direction == "up") snake[0].y = (16 * box)
+    else if (snake[0].x < 0 && direction == "left") snake[0].x = (16 * box)
+    else if (snake[0].y > (15 * box) && direction == "down") snake[0].y = 0
+    else if (snake[0].y < 0 && direction == "up") snake[0].y = (16 * box)
 }
 
 function drawFood () {
@@ -33,10 +33,23 @@ function drawFood () {
 
 function toEat() {
     if (snake[0].x == food.x && snake[0].y == food.y) {
-        snake.unshift({ x: box, y: box })
+        // snake.unshift({ x: box, y: box })
         food = { x: Math.floor(Math.random() * 15 + 1) * box, y: Math.floor(Math.random() * 15 + 1) * box }
-    } else {
-        snake.pop()
+        return;
+    }
+    
+    snake.pop()
+}
+
+function checkGameOver() {
+    for (let i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(game)
+            alert("Game over :(")
+            snake = []
+            snake[0] = { x: 8 * box, y: 8 * box }
+            game = setInterval(initGame, 100)
+        }
     }
 }
 
@@ -44,13 +57,14 @@ document.addEventListener('keydown', update)
 
 function update (event) {
     if (event.keyCode == 37 && direction != "right") direction = "left"
-    if (event.keyCode == 38 && direction != "down") direction = "up"
-    if (event.keyCode == 39 && direction != "left") direction = "right"
-    if (event.keyCode == 40 && direction != "up") direction = "down"
+    else if (event.keyCode == 38 && direction != "down") direction = "up"
+    else if (event.keyCode == 39 && direction != "left") direction = "right"
+    else if (event.keyCode == 40 && direction != "up") direction = "down"
 }
 
 function initGame() {
-    checkCollision() 
+    checkCollision()
+    checkGameOver() 
     initializeCanvas()
     drawSnake()
     drawFood()
@@ -58,25 +72,14 @@ function initGame() {
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    switch (direction) {
-        case "right":
-            snakeX += box
-            break
-        case "left":
-            snakeX -= box
-            break;
-        case "up":
-            snakeY -= box
-            break
-        case "down":
-            snakeY += box
-            break
-    }
+    if (direction == "right") snakeX += box
+    else if (direction == "left") snakeX -= box
+    else if (direction == "up") snakeY -= box
+    else if (direction == "down") snakeY += box
 
     toEat()
-    let newSnake = { x: snakeX, y: snakeY }
-    
-    snake.unshift(newSnake)
+    let newHead = { x: snakeX, y: snakeY }
+    snake.unshift(newHead)
 }
 
-let jogo = setInterval(initGame, 100)
+let game = setInterval(initGame, 100)
