958b3c11ada7803845244b7be8519f349a4a6086
Lais Frigério <laisfrigerio@hotmail.com>
2020-06-11 11:32:26 -0300

    Add snake
    Signed-off-by: Lais Frigério <laisfrigerio@hotmail.com>

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const canvas = document.getElementById("snake")
 const context = canvas.getContext("2d")
 const box = 32
 
+let snake = []
+snake[0] = { x: 8 * box, y: 8 * box }
+
 function initializeCanvas () {
     context.fillStyle = "#2a2a2a";
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
+function drawSnake () {
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = "#9c27b0"
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
 initializeCanvas()
+drawSnake()
