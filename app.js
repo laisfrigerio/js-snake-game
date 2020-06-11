e31c60364be00baa880f8dd27b99ea6f748e02d6
Lais Frigério <laisfrigerio@hotmail.com>
2020-06-11 12:02:01 -0300

    Draw food
    Signed-off-by: Lais Frigério <laisfrigerio@hotmail.com>

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const context = canvas.getContext("2d")
 const box = 32
 
 let direction = "right"
+let food = { x: Math.floor(Math.random() * 15 + 1) * box, y: Math.floor(Math.random() * 15 + 1) * box }
 let snake = []
 snake[0] = { x: 8 * box, y: 8 * box }
 
@@ -19,25 +20,31 @@ function drawSnake () {
 }
 
 function checkCollision () {
-    if (snake[0].x > (15 * box) && direction === "right") snake[0].x = 0
-    if (snake[0].x < 0 && direction === "left") snake[0].x = (16 * box)
-    if (snake[0].y > (15 * box) && direction === "down") snake[0].y = 0
-    if (snake[0].y < 0 && direction === "up") snake[0].y = (16 * box)
+    if (snake[0].x > (15 * box) && direction == "right") snake[0].x = 0
+    if (snake[0].x < 0 && direction == "left") snake[0].x = (16 * box)
+    if (snake[0].y > (15 * box) && direction == "down") snake[0].y = 0
+    if (snake[0].y < 0 && direction == "up") snake[0].y = (16 * box)
+}
+
+function drawFood () {
+    context.fillStyle = "red"
+    context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener('keydown', update)
 
 function update (event) {
-    if (event.keyCode === 37 && direction !== "right") direction = "left"
-    if (event.keyCode === 38 && direction !== "down") direction = "up"
-    if (event.keyCode === 39 && direction !== "left") direction = "right"
-    if (event.keyCode === 40 && direction !== "up") direction = "down"
+    if (event.keyCode == 37 && direction != "right") direction = "left"
+    if (event.keyCode == 38 && direction != "down") direction = "up"
+    if (event.keyCode == 39 && direction != "left") direction = "right"
+    if (event.keyCode == 40 && direction != "up") direction = "down"
 }
 
 function initGame() {
     checkCollision() 
     initializeCanvas()
     drawSnake()
+    drawFood()
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
